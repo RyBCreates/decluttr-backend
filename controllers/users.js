097c08a638067c21b19d475df9b725b29351bc1c
Gtavarez <giselltavarez@gmail.com>
2025-09-14 21:04:39 -0400

    adding login function

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const {}
 
 // Get User by ID
 const getCurrentUser = (req, res) => {
@@ -8,5 +10,28 @@ const getCurrentUser = (req, res) => {
     res.send(user);
   });
 };
+// Login function
+const login = (req, res) => {
+  const { email, password } = req.body;
 
-module.exports = { getCurrentUser };
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect password or email"));
+      }
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        // the hashes didn't match, rejecting the promise
+        return Promise.reject(new Error("Incorrect password or email"));
+      }
+      // successful authentication
+      res.send({ message: "Everything good!" });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+};
+
+module.exports = { getCurrentUser, login };
