3901b2a9e41e22a621e308e02c620d45f77fdb7a
Gtavarez <giselltavarez@gmail.com>
2025-09-15 21:25:26 -0400

    modifting user on models

--- models/user.js
+++ models/user.js
@@ -12,28 +12,38 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    validate:{
-      validator:(v)=> isEmail(v),
-      message: "wrong email format"
-    }
+    validate: {
+      validator: (v) => isEmail(v),
+      message: "wrong email format",
+    },
   },
+  password: {
+    type: String,
+    required: true,
+    select: false, // do not return password field by default
+  },
+  level: { type: String, enum: ["basic", "admin"], default: "basic" },
+  xp: { type: Number, default: 0 },
+  gems: { type: Number, default: 0 },
+  steak: { type: Number, default: 0 },
 });
-userSchema.statics.findUserByCredentials = function findUserByCredentials (email, password) {
-  return this.findOne({ email })
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error('Incorrect email or password'));
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error("Incorrect email or password"));
+    }
+
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error("Incorrect email or password"));
       }
 
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new Error('Incorrect email or password'));
-          }
-
-          return user; // now user is available
-        });
+      return user; // now user is available
     });
+  });
 };
 
 module.exports = mongoose.model("User", userSchema);
