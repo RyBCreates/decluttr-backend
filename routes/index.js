d83d736552b3eacdda4b8242b52feb3f10c246f9
Ryan B <bombingrun3@gmail.com>
2025-09-16 01:18:55 -0400

    setup GET function for All achievements

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userRouter = require("./user");
 const taskRouter = require("./tasks");
+const achievementRouter = require("./achievements");
 
 router.use("/users", userRouter);
 router.use("/tasks", taskRouter);
+router.use("/achievements", achievementRouter);
 
 module.exports = router;
