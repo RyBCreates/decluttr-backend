38c5ab4a2abf5baa86c72fb9c969a9217d6284c3
Ryan B <bombingrun3@gmail.com>
2025-09-17 13:26:31 -0400

    On register initializes achievements array

--- models/userAchievement.js
+++ models/userAchievement.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userAchievementSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   achievementId: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     ref: "Achievement",
     required: true,
   },
